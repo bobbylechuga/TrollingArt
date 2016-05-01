8cdeaf9a9d74c0e1b465746a879a1e4740c925b7
Pino <pino.jose@gmail.com>
2016-05-01 13:55:36 -0300

    corregido bug que no dejaba actualizar nombre del autor

--- themes/trollingarttheme/functions.php
+++ themes/trollingarttheme/functions.php
@@ -253,8 +253,14 @@ function numericPagination() {
 }
 
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
-/* 																	Nuevos campos para usuarios                         */
+/* 									Nuevos campos para usuarios                         */
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+function get_user_role($id)
+{
+    $user = new WP_User($id);
+    return array_shift($user->roles);
+}
 
 function custom_user_profile_fields($user){
     if(is_object($user)) {
@@ -288,43 +294,47 @@ add_action( 'edit_user_profile', 'custom_user_profile_fields' );
 add_action( "user_new_form", "custom_user_profile_fields" );
 
 function save_custom_user_profile_fields($user_id){
+	$role = get_user_role($user_id);
+		
     # again do this only if you can
     if(!current_user_can('manage_options'))
         return false;
 
     # save my custom field
 
-		$urlbio = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts|pageimages&exlimit=max&explaintext&exintro&pithumbsize=300&titles=".$_POST['company'];
+	$urlbio = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts|pageimages&exlimit=max&explaintext&exintro&pithumbsize=300&titles=".$_POST['company'];
 
-		$str = file_get_contents($urlbio);
-		$parsed_json = json_decode($str);
-		foreach($parsed_json->query->pages as $k) {
-		    $bioArtist = $k->extract;
-				$nickName = $k->title;
-				if (!empty($k->thumbnail)) {
-					$arrayThum[] = $k->thumbnail;
-					foreach($arrayThum as $q) {
-						$wikiProfile = $q->source;
-						//echo $q->source;
-					}
-		    }
-		}
-		if (strpos($nickName, ' ') !== false) {
-	  	$nameLastname = explode(" ", $nickName);
-			$name = $nameLastname[0];
-			$lastname = $nameLastname[1];
-		}else {
-			$name = $nickName;
-			$lastname = $nickName;
-		}
+	$str = file_get_contents($urlbio);
+	$parsed_json = json_decode($str);
+	foreach($parsed_json->query->pages as $k) {
+	    $bioArtist = $k->extract;
+			$nickName = $k->title;
+			if (!empty($k->thumbnail)) {
+				$arrayThum[] = $k->thumbnail;
+				foreach($arrayThum as $q) {
+					$wikiProfile = $q->source;
+					//echo $q->source;
+				}
+	    }
+	}
+	if (strpos($nickName, ' ') !== false) {
+  	$nameLastname = explode(" ", $nickName);
+		$name = $nameLastname[0];
+		$lastname = $nameLastname[1];
+	}else {
+		$name = $nickName;
+		$lastname = $nickName;
+	}
 
-    update_user_meta($user_id, 'company', $_POST['company']);
-		update_user_meta($user_id, 'wikiPicfile', $wikiProfile);
-		update_user_meta($user_id, 'description', $bioArtist);
+	update_user_meta($user_id, 'company', $_POST['company']);
+	update_user_meta($user_id, 'wikiPicfile', $wikiProfile);
+	update_user_meta($user_id, 'description', $bioArtist);
+	if ($role == "WikiArtist") { 
 		update_user_meta($user_id, 'first_name', $name);
 		update_user_meta($user_id, 'last_name', $lastname);
 		update_user_meta($user_id, 'nickname', $nickName);
 		update_user_meta($user_id, 'display_name', $nickName);
+	}
 }
 add_action('user_register', 'save_custom_user_profile_fields');
 add_action('profile_update', 'save_custom_user_profile_fields');
